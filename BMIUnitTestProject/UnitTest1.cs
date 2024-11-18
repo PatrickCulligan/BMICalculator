441f5a66b06d10b99a76a3a1037121c30315bd15
Patrick Culligan <X00218712@mytudublin.ie>
2024-11-18 17:46:44 +0000

    Push Unit Test

--- BMIUnitTestProject/UnitTest1.cs
+++ BMIUnitTestProject/UnitTest1.cs
@@ -10,14 +10,14 @@ namespace BMIUnitTestProject
         [TestMethod]
         public void TestMethod1()
         {
-            BMI bmi = new BMI() { WeightStones = 12, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
-            Assert.AreEqual(bmi.BMICategory, BMICategory.Normal);
+            BMI bmi = new BMI() { WeightStones = 12, WeightPounds = 0, HeightFeet = 5, HeightInches = 10, Age=12 };
+            Assert.AreEqual(bmi.BMICategory, BMICategory.Overweight);
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            BMI bmi = new BMI() { WeightStones = 15, WeightPounds = 0, HeightFeet = 5, HeightInches = 10 };
+            BMI bmi = new BMI() { WeightStones = 15, WeightPounds = 0, HeightFeet = 5, HeightInches = 10, Age = 29 };
             Assert.AreEqual(bmi.BMICategory, BMICategory.Obese);
         }
     }
